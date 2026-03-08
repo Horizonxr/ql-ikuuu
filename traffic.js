5adca4bcd2bdca2958b60ec3c39d4b0fc7f4d21c
Horizonxr <60167720+Horizonxr@users.noreply.github.com>
2026-03-08 20:51:22 +0800

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -6,27 +6,14 @@ async function run() {
 
   if (directCookie) {
     let msg = '📌 Cookie 模式流量查询'
+
     const arr = await getTraffic(directCookie)
+    console.log('流量结果：', arr)
     msg += '\n' + arr.join('\n')
-    await notify.sendNotify('iKuuu VPN 今日流量统计', msg)
-  } else {
-    const [emailList, pwdList] = await getEmailAndPwdList()
-    const messages = []
-    for (let i = 0; i < emailList.length; i++) {
-      const email = emailList[i]
-      const pwd = pwdList[i]
-      let msg = '邮箱：' + emailList[i]
-      const cookie = await getCookie(email, pwd)
-      if (cookie.includes('登录失败')) {
-        msg += '\n' + cookie
-        messages.push(msg)
-        continue
-      }
-      const arr = await getTraffic(cookie)
-      msg += '\n' + arr.join('\n')
-      messages.push(msg)
-    }
-    await notify.sendNotify('iKuuu VPN 今日流量统计', messages.join('\n\n========================\n\n'))
+
+    console.log('准备发送通知，内容：\n' + msg)
+    await notify.sendNotify('iKuuu VPN 流量统计', msg)
+    console.log('通知发送完成')
   }
 }
 
