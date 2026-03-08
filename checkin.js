44ae4d8182ec06813a74c9ca20a8bc736e5242f1
Horizonxr <60167720+Horizonxr@users.noreply.github.com>
2026-03-08 20:56:37 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const { getCookie, getTraffic, getEmailAndPwdList, getDirectCookie } = require('./utils')
-const notify = require('./sendNotify')
+await QLAPI.notify('iKuuu VPN 流量统计', msg)
 
 const host = 'https://ikuuu.nl'
 const checkinURL = host + '/user/checkin'
